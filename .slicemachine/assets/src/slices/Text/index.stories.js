7c0e7876c6711f5cca7f355fc5ff6df6d71d356d
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-09-15 14:03:42 -1000

    chore: add starter Slice

---
+++ .slicemachine/assets/src/slices/Text/index.stories.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import MyComponent from '../../../../../src/slices/Text';
+
+export default {
+  title: 'src/slices/Text'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"text","items":[],"primary":{"text":[{"type":"paragraph","text":"Sit amet elit laboris duis.","spans":[]}]},"id":"_DefaultSlice"}} />
+_DefaultSlice.storyName = 'Default slice'
+
